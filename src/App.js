01de241aa4d2215088187cb33aa956b81ad4ce60
Mario <mariomatheuspombal@rede.ulbra.br>
2023-05-24 17:06:33 -0300

    Projeto Funcionando

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./utils/navbar/navbar";
+import User from "./components/users/user";
+import react, {useEffect} from "react";
+import AddUser from "./components/users/addusers";
+import './components/users/users.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [users, setUsers] = react.useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        await fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(json => setUsers(json))
+            .catch(error => console.log(error));
+    };
+
+    const onAdd = async (name, email, website) => {
+        await fetch('https://jsonplaceholder.typicode.com/users', {
+            method: 'POST',
+            body: JSON.stringify({
+                name: name,
+                email: email,
+                website: website
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then(response => response.json())
+            .then(json => setUsers([...users, json]))
+            .catch(error => console.log(error));
+    }
+
+    const onEdit = async (id, name, email, website) => {
+        await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                name: name,
+                email: email,
+                website: website
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then(response => response.json())
+            .then(json => setUsers(users.map(user => user.id === id ? json : user)))
+            .catch(error => console.log(error));
+    }
+
+    const onRemove = async (id) => {
+        await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+            method: 'DELETE',
+        })
+            .then(response => response.json())
+            .then(json => setUsers(users.filter(user => user.id !== id)))
+            .catch(error => console.log(error));
+    }
+
+
+    return (
+        <div>
+            <Navbar/>
+            <div>
+                <h1 className={'h1'}>Lista de Usuários</h1>
+                <AddUser onAdd={onAdd}/>
+                {users.map(user => (
+                    <User key={user.id} id={user.id} name={user.name} email={user.email} website={user.website}
+                          onRemove={onRemove} onUpdate={onEdit}/>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/utils/navbar/navbar.js
@@ -0,0 +1,23 @@
+import './navbar.css';
+
+export default function Navbar() {
+    return (
+        <section className={'section'}>
+            <div className={'navbardiv'}>
+                <div>
+                    <h1 className={'h1navbar'}>Navbar do Mario</h1>
+                </div>
+                <div className={'div'}>
+                    <button className={'button'}>Button</button>
+                </div>
+                <div className={'div'}>
+                    <button className={'button'}>Button</button>
+                </div>
+                <div className={'div'}>
+                    <button className={'button'}>Button</button>
+                </div>
+            </div>
+        </section>
+)
+    ;
+}
